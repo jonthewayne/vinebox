5565f59d6f125aa2de5fdc018d5f3f856db2985f
Jon Shumate <jonthewayne@gmail.com>
2013-04-06 00:27:04 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vinebox::Application.config.secret_token = '1e6f80ce5ad55cb0dc97883dba7d80c3c6d0bef10e19fa409c6f53c8b2477dddf42ded7cad8b2099c88eb146873f67f1ad92f2d1dc380eda4700fcef22dcbf8c'
